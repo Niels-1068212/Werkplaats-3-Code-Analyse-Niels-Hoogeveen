fb962e41c418c23600db3ee1854d6a3e23c22d42
Niels-1068212 <143613483+Niels-1068212@users.noreply.github.com>
2024-09-29 10:25:42 +0200

    CHANGE 3 - fix - Middleware, Content Security Policy (CSP)

--- root/root/settings.py
+++ root/root/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'root.middleware.ContentSecurityPolicyMiddleware',
 ]
 
 ROOT_URLCONF = 'root.urls'
